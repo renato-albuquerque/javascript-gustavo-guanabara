0dd4232b42f8c23fa74e5010ef993cbd3c1dcf1d
renato-albuquerque <renatomalb83@gmail.com>
2023-03-22 15:05:53 -0300

    added class 14

---
+++ aula14/environment.js
@@ -0,0 +1,16 @@
+// WHILE and FOR: Same result.
+
+/* let c = 1
+
+while(c <= 5) {
+    console.log(c);
+    c++
+} */
+
+console.log("Start");
+
+for(let c = 1; c <=5; c++) {
+    console.log(c);
+}
+
+console.log("End");
